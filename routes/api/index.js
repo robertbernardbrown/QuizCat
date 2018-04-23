2d7a0fe286576d15a7e8aa1fddfed9e878a0aef6
Bob Brown <robertbernardbrown@gmail.com>
2018-04-23 17:16:35 -0400

    formats starter route files

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const articleRoutes = require("./articles");
+const navRoutes = require("./articles");
 
 // Article routes
-router.use("/articles", articleRoutes);
+router.use("/navigation", navRoutes);
 
 module.exports = router;
