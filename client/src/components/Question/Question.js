d6368b732e635fc9c700526856972567fd2cf4d2
Bob Brown <robertbernardbrown@gmail.com>
2018-04-24 10:34:18 -0400

    decodes questions from API

--- client/src/components/Question/Question.js
+++ client/src/components/Question/Question.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Question.css";
 
+var parser = new DOMParser();
+
 const Question = props => (
     <div className="question">
-        {props.questions.map((cur, i) => (
-            <p className={i} key={i}>{cur.question}</p>
-        ))}
+        {props.questions.map((cur, i) => {
+            let dom = parser.parseFromString(cur.question, 'text/html');
+            let decodedString = dom.body.textContent;
+            return (<p className={i} key={i}>{decodedString}</p>);
+        })}
     </div>
 )
 
