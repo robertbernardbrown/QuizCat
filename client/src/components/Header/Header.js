a312d82a583055ddeab7c6d83bb3e8c93dff61c0
Bob Brown <robertbernardbrown@gmail.com>
2018-04-27 16:32:55 -0400

    edits header style to be a link

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -6,7 +6,7 @@ import "react-bootstrap";
 const Header = () => (
     <div className="jumbotron">
         <div className="container">
-            <h1 className="display-4 text-center">QuizCat</h1>
+            <a href="/"><h1 className="display-4 text-center">QuizCat</h1></a>
         </div>
     </div>
 )
