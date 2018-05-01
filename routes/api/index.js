f3d368cc6c64c0b2bdd75a1d13c256b20cbf6c4c
Bob Brown <robertbernardbrown@gmail.com>
2018-05-01 11:22:06 -0400

    fixes merge issues

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,14 +1,8 @@
 const router = require("express").Router();
-<<<<<<< HEAD
 const dataRoutes = require("./articles");
 
 // Article routes
 router.use("/data", dataRoutes);
-=======
-const navRoutes = require("./articles");
 
-// Article routes
-router.use("/navigation", navRoutes);
->>>>>>> master
 
 module.exports = router;
