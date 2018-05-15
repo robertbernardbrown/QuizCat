cb46490ede409043bacd3c402143af8d5d05a8ac
Bob Brown <robertbernardbrown@gmail.com>
2018-05-15 12:02:11 -0400

    makes change to models for questions

--- models/questions.js
+++ models/questions.js
@@ -5,9 +5,9 @@ const questionsSchema = new Schema({
   question: { type: String, required: true },
   correct_answer: { type: String, required: true },
   incorrect_answers: { type: Array, required: true },
-  category: { type: String, required: true },
-  type: { type: String, required: true },
-  difficulty: { type: String, required: true },
+  category: { type: String },
+  type: { type: String },
+  difficulty: { type: String },
   dateAccessed: { type: Date, default: Date.now }
 });
 
