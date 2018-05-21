5ed75a12976d3231fa4053440a114800b04801ab
Bob Brown <robertbernardbrown@gmail.com>
2018-05-21 16:14:37 -0400

    adds some tests for smoke testing comps

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,99 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+import About from "./components/About";
+import Answer from "./components/Answer";
+import Contact from "./components/Contact";
 import App from "./App";
+import ReactDOM from "react-dom";
+Enzyme.configure({ adapter: new Adapter() })
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+beforeAll(() => {
+  global.localStorage = {
+     i2x_token: 'someToken',
+     getItem: function () {
+        return 'someToken'
+     }
+  };
 });
+
+describe("About Component", () => {
+  it("About shallow exists", () => {
+    const about = shallow(<About/>)
+    console.log(about);
+    expect(about.find('div.about')).to.have.length(1);
+  });
+
+  it('About renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<About />, div);
+  });
+
+  it("About shallow contains paragraph", () => {
+    const about = shallow(<About/>)
+    console.log(about);
+    expect(about.find("div")).toHaveLength(1);
+  });
+  
+  it("about specific exists", () => {
+    const about = <About/>
+    console.log(about);
+    expect(<About/>).toHaveLength(1);
+  });
+});
+
+describe("Answer Component", () => {
+  const options = ["hi", "there"];
+
+  it("Answer shallow exists", () => {
+    const answer = shallow(<Answer options={options}/>)
+    console.log(answer);
+    expect(answer).toHaveLength(1);
+  });
+
+  it('Answer renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Answer options={options}/>, div);
+  });
+  
+  it("Answer specific exists", () => {
+    const answer = <Answer options={options}/>
+    console.log(answer);
+    expect(answer).toHaveLength(1);
+  });
+});
+
+describe("Contact Component", () => {
+  it("Contact shallow exists", () => {
+    const contact = shallow(<Contact/>)
+    console.log(contact);
+    expect(contact).toHaveLength(1);
+  });
+
+  it('Contact renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Contact/>, div);
+  });
+  
+  it("Contact shallow exists", () => {
+    const contact = <Contact/>
+    console.log(contact);
+    expect(contact).toHaveLength(1);
+  });
+});
+
+describe("App Component", () => {
+  it('app shallow renders without crashing', () => {
+    const app = shallow(<App />);
+    console.log(app)
+    expect(app).toHaveLength(1);
+  });
+  
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+});
+
+
+
