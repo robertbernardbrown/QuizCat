0d9a79e64001d3306906bb37a9f99e1d13a78f1c
Bob Brown <robertbernardbrown@gmail.com>
2018-05-22 15:45:37 -0400

    fixes render smoke tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, { shallow, render } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
 import About from "./components/About";
 import Answer from "./components/Answer";
@@ -17,11 +17,27 @@ beforeAll(() => {
   };
 });
 
+const setup = propOverrides => {
+  const props = Object.assign({
+    completedCount: 0,
+    activeCount: 0,
+    onClearCompleted: jest.fn(),
+  }, propOverrides)
+
+  const wrapper = shallow(<Footer {...props} />)
+
+  return {
+    props,
+    wrapper,
+    clear: wrapper.find('.clear-completed'),
+    count: wrapper.find('.todo-count'),
+  }
+}
+
 describe("About Component", () => {
   it("About shallow exists", () => {
     const about = shallow(<About/>)
-    console.log(about);
-    expect(about.find('div.about')).to.have.length(1);
+    expect(about).toHaveLength(1);
   });
 
   it('About renders without crashing', () => {
@@ -29,17 +45,21 @@ describe("About Component", () => {
     ReactDOM.render(<About />, div);
   });
 
-  it("About shallow contains paragraph", () => {
-    const about = shallow(<About/>)
-    console.log(about);
-    expect(about.find("div")).toHaveLength(1);
+  it("About shallow contains content", () => {
+    const about = shallow(<About/>).find("div#about");
+    const innerAbout = shallow(<About/>).find("div#inner-about-div");
+    const li = shallow(<About/>).find("li");
+    expect(about).toHaveLength(1);
+    expect(innerAbout).toHaveLength(1);
+    expect(li).toHaveLength(6);
   });
   
-  it("about specific exists", () => {
-    const about = <About/>
+  it("About renders", () => {
+    const about = render(<About/>);
     console.log(about);
-    expect(<About/>).toHaveLength(1);
+    expect(about).toHaveLength(1);
   });
+    
 });
 
 describe("Answer Component", () => {
@@ -56,8 +76,8 @@ describe("Answer Component", () => {
     ReactDOM.render(<Answer options={options}/>, div);
   });
   
-  it("Answer specific exists", () => {
-    const answer = <Answer options={options}/>
+  it("Answer renders", () => {
+    const answer = render(<Answer options={options}/>);
     console.log(answer);
     expect(answer).toHaveLength(1);
   });
@@ -75,24 +95,32 @@ describe("Contact Component", () => {
     ReactDOM.render(<Contact/>, div);
   });
   
-  it("Contact shallow exists", () => {
-    const contact = <Contact/>
+  it("Contact renders", () => {
+    const contact = render(<Contact/>);
     console.log(contact);
     expect(contact).toHaveLength(1);
   });
 });
 
 describe("App Component", () => {
-  it('app shallow renders without crashing', () => {
+  it('App shallow renders without crashing', () => {
     const app = shallow(<App />);
     console.log(app)
     expect(app).toHaveLength(1);
   });
-  
-  it('renders without crashing', () => {
+
+  it('App renders without crashing', () => {
     const div = document.createElement('div');
+    console.log(div);
     ReactDOM.render(<App />, div);
   });
+
+  it("App renders", () => {
+    const app = render(<App/>);
+    console.log(app);
+    expect(app).toHaveLength(1);
+  });
+  
 });
 
 
