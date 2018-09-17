8f853f7246745cbe7f45e10cec7290a757b3c9fc
Bob Brown <robertbernardbrown@gmail.com>
2018-09-17 11:52:18 -0400

    adds display styles to sign in and sign up forms

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -5,43 +5,41 @@ import { Link } from 'react-router-dom';
 import FbLogin from '../FbLogin';
 
 const LoginForm = ({onSubmit,onChange,errors,successMessage,user,toggleAuthenticateStatus,setUser,authenticated}) => (
-  <div className="container">
-    <div id="inner-credential-div">
-      <h2 className="login-heading">Login</h2>
-      <FbLogin onClick={() => setUser(user)} authenticated={authenticated} toggleAuthenticateStatus={toggleAuthenticateStatus}/>
-      <form action="/" onSubmit={onSubmit}>
+  <div id="inner-credential-div">
+    <h2 className="login-heading">Login</h2>
+    <FbLogin onClick={() => setUser(user)} authenticated={authenticated} toggleAuthenticateStatus={toggleAuthenticateStatus}/>
+    <form action="/" onSubmit={onSubmit}>
 
-        {successMessage && <p className="success-message">{successMessage}</p>}
-        {errors.summary && <p className="error-message">{errors.summary}</p>}
+      {successMessage && <p className="success-message">{successMessage}</p>}
+      {errors.summary && <p className="error-message">{errors.summary}</p>}
 
-        <div className="field-line">
-          <input
-              placeholder={errors.email ? errors.email.toString() : "Email"}
-              name="email"
-              className={errors.email ? "has-error" : ""}
-              onChange={onChange}
-              value={user.email}
-          />
-        </div>
+      <div className="field-line">
+        <input
+            placeholder={errors.email ? errors.email.toString() : "Email"}
+            name="email"
+            className={errors.email ? "has-error" : ""}
+            onChange={onChange}
+            value={user.email}
+        />
+      </div>
 
-        <div className="field-line">
-          <input
-              placeholder={errors.password ? errors.password.toString() : "Password"}
-              type="password"
-              name="password"
-              onChange={onChange}
-              className={errors.password ? "has-error" : ""}
-              value={user.password}
-          />
-        </div>
+      <div className="field-line">
+        <input
+            placeholder={errors.password ? errors.password.toString() : "Password"}
+            type="password"
+            name="password"
+            onChange={onChange}
+            className={errors.password ? "has-error" : ""}
+            value={user.password}
+        />
+      </div>
 
-        <div className="button-line">
-          <button className="btn btn-primary btn-block" type="submit">Log-In</button>
-        </div>
+      <div className="button-line">
+        <button className="btn btn-primary btn-block" type="submit">Log-In</button>
+      </div>
 
-        <p>Don't have an account? <Link to={'/signup'} id="sign">Create&nbsp;one</Link>.</p>
-      </form>
-    </div>
+      <p>Don't have an account? <Link to={'/signup'} id="sign">Create&nbsp;one</Link>.</p>
+    </form>
   </div>
 );
 
